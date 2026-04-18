9e0472a8e46521fed1fc19b2aa7f01a7ddb583da
Jiao0216 <158863026+Jiao0216@users.noreply.github.com>
2026-04-18 15:40:07 -0700

    添加购物车

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
  * jwt令牌校验的拦截器
  */
@@ -47,8 +48,8 @@ public class JwtTokenUserInterceptor implements HandlerInterceptor {
         try {
             log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getUserSecretKey(), token);
-            Long userId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
-            log.info("当前用户id：", userId);
+            Long userId = Long.valueOf(claims.get(JwtClaimsConstant.USER_ID).toString());
+            log.info("当前用户的id：", userId);
             BaseContext.setCurrentId(userId);
             //3、通过，放行
             return true;
